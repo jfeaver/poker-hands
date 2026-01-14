89c7d4d2f0cef90f45298d3820c8cde3a40161be
Nathan Feaver <nathan.feaver@gmail.com>
2026-01-14 12:17:39 -0500

    remove weather forecast endpoint

--- Program.cs
+++ Program.cs
@@ -41,25 +41,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapPost("/hand_comparisons", (PlayerHand[] players) =>
 {
     if (players is null || players.Length == 0)
@@ -94,11 +75,6 @@ app.MapPost("/hand_comparisons", (PlayerHand[] players) =>
 .WithName("CreateHandComparison");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 
 record PotWinner(string PlayerId, PokerHand.HandRank Title, Card[] Hand) { }
 
